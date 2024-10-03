a424493b0aa956f27f148f13eaa7feba82fabea8
Alex Sim <simweixuan@hotmail.com>
2024-10-03 23:55:29 +0800

    Delete apikeys.py

--- apikeys.py
+++
@@ -1,7 +0,0 @@
-# api_keys.py
-
-VIRUSTOTAL_API_KEY = "8dfe3cca72df958c0715f4f01d466b49d05907920dd7c26290a8c37f571f35c0"
-SHODAN_API_KEY = "lkY0ng0XMo29zEhzyw3ibQfeEBxghwPF"
-
-# You can add other API keys here as needed
-# OTHER_API_KEY = "your_other_api_key_here"
